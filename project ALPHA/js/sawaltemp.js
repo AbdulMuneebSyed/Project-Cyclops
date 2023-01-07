da46c7da0a7ead8f80d17ddc1a76c2411ef01ffc
AbdulMuneebSyed <119971617+AbdulMuneebSyed@users.noreply.github.com>
2023-01-08 01:16:28 +0530

    Add files via upload

---
+++ project ALPHA/js/sawaltemp.js
@@ -0,0 +1,72 @@
+// questions
+let questions = [
+  {
+    numb: 1,
+    question: "Question idher poochna",
+    answer: "yeh correct hai",
+    options: [
+        "yeh ghalat hai",
+        "yeh bhi ghalat hai",
+        "yeh correct hai",
+        "yeh toh bilkul ghalat hai"
+    ]
+},
+    {
+        numb: 2,
+        question: "Question idher poochna",
+        answer: "yeh correct hai",
+        options: [
+            "yeh ghalat hai",
+            "yeh correct hai",
+            "yeh bhi ghalat hai",
+            "yeh toh bilkul ghalat hai"
+        ]
+  },
+  {
+    numb: 3,
+    question: "Question idher poochna",
+    answer: "yeh correct hai",
+    options: [
+        "yeh ghalat hai",
+        "yeh bhi ghalat hai",
+        "yeh toh bilkul ghalat hai",
+        "yeh correct hai",
+    ]
+},
+{
+  numb: 4,
+  question: "Question idher poochna",
+  answer: "yeh correct hai",
+  options: [
+      "yeh ghalat hai",
+      "yeh bhi ghalat hai",
+      "yeh correct hai",
+      "yeh toh bilkul ghalat hai"
+  ]
+},
+{
+  numb: 5,
+  question: "Question idher poochna",
+  answer: "yeh correct hai",
+  options: [
+      "yeh correct hai",
+      "yeh ghalat hai",
+      "yeh bhi ghalat hai",
+      "yeh toh bilkul ghalat hai"
+  ]
+}
+  //yeh format mien aage likhte huwe chale ja sakte;
+  //numb ku initialize karne mt bhulo;
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
